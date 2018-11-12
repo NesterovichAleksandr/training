c910eb282dffb5b7186c670b969c9bf06542f498
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-11-12 11:21:15 +0300

    quick sort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -5,19 +5,26 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] array = {5, 7, 8, 4, 1, 3, 7, 8, 9, 5};
+        int size = (int) (1 + (Math.random() * 100));
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
         System.out.println(Arrays.toString(array));
         quickSort(array);
         System.out.println(Arrays.toString(array));
     }
 
-    static void quickSort(int[] array) {
+    private static void quickSort(int[] array) {
         int leftIndex = 0;
         int rightIndex = array.length - 1;
         doSort(array, leftIndex, rightIndex);
     }
 
-    static void doSort(int[] array, int start, int end) {
+    private static void doSort(int[] array, int start, int end) {
+        if (start >= end) {
+            return;
+        }
         int leftIndex = start;
         int rightIndex = end;
         int middle = (leftIndex + rightIndex) / 2;
