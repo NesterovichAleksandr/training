92342ca845b262af246c96a5cb63355c9a17f12e
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-11-19 16:27:34 +0300

    Task 1

---
+++ src/books/philosophyjava/chapter2/Task1.java
@@ -0,0 +1,12 @@
+package books.philosophyjava.chapter2;
+
+public class Task1 {
+
+    private static int fild1;
+    private static char fild2;
+
+    public static void main(String[] args) {
+        System.out.println(fild1);
+        System.out.println(fild2);
+    }
+}
