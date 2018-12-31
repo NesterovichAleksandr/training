3f7edb91359068dd120a3bcdb8971fb041aadbb5
Aleksandr Nesterovich <nesterovich.aleksandr.a@gmail.com>
2019-01-01 00:01:42 +0300

    С НОВЫМ ГОДОМ! :)

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,6 +3,8 @@ package hello;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("***********************");
+        System.out.println("*  С НОВЫМ ГОДОМ! :)  *");
+        System.out.println("***********************");
     }
 }
